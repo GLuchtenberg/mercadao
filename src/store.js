85383d218ee7b93a1f1fcea22e15b2eb43e7ed8c
Gabriel Luchtenberg <gabriel_luchtenberg@edu.univali.br>
2018-04-09 13:13:53 -0300

    vuex example

--- src/store.js
+++ src/store.js
@@ -5,12 +5,39 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    todos: [
+      {
+        task: 'teste',
+        completed: false
+      },
+      {
+        task: 'teste2',
+        completed: false
+      },
+      {
+        task: 'teste3',
+        completed: false
+      }
+    ]
   },
   mutations: {
-
+    addTodo: (state, payload) => {
+      payload.completed = true
+      state.todos.unshift(payload)
+    },
+    toggleTodo: (state, payload) => {
+      state.todos = state.todos.map(t => {
+        if (t.task === payload) { return { task: t.task, completed: !t.completed } }
+        return t
+      })
+    }
   },
   actions: {
-
+    addTodo: ({ commit }, payload) => {
+      commit('addTodo', payload)
+    }
+  },
+  getters: {
+    todos: state => { return state.todos }
   }
 })
